8c5bc53414126e303d3fd0e824e831f48ebd759a
developers <developers@sevencomp.pl>
2021-06-02 11:21:24 +0200

    deploy

--- note.md
+++
@@ -1,6 +0,0 @@
-TO DO:
-
-1. Powiadomienia
-    * Ogłoszenia podobne do dodanego
-    * Newsletter
-    * 
--- resources/views/partials/facebook-meta.blade.php
+++
@@ -1,6 +0,0 @@
-<meta property="og:url" content="{{url('/')}}"/>
-<meta property="og:type" content="article"/>
-<meta property="og:title" content="Sarz.pl - Społeczna Akcja Ratowania Zwierząt"/>
-<meta property="og:description" content="W pełni darmowa aplikacja z bazą danych zaginionych oraz znalezionych zwierząt."/>
-<meta property="og:image" content="{{url('images/facebook-share.jpg')}}"/>
-<meta property="fb:app_id" content="882750211905623"/>
